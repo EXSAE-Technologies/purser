f73ad8c9d7f15e8edd40489c25c6eeb3efeea056
fshangala <fshangala@gmail.com>
2023-03-03 19:23:51 +0200

    login logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,11 @@ from rest_framework import routers
 from django.urls import path
 from .views import (
     UserViewSet,
-    AccountLogin
+    AccountLogin,
+    AccountProfile,
+    AccountMain
 )
+from django.contrib.auth.views import logout_then_login
 
 router = routers.DefaultRouter()
 router.register(r'user',UserViewSet,basename="user")
@@ -11,4 +14,8 @@ router.register(r'user',UserViewSet,basename="user")
 app_name="accounts"
 urlpatterns = [
     path('login/',AccountLogin.as_view(),name="login"),
+    path('logout/',logout_then_login,name="logout"),
+    path('id/<pk>/',AccountProfile.as_view(),name="profile"),
+    path('',AccountMain.as_view(),name="main"),
+    path('profile/',AccountMain.as_view(),name="profile2")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer
 from rest_framework import permissions
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import DetailView, RedirectView
+from django.urls import reverse
 
 # Create your views here.
 class UserViewSet(ModelViewSet):
@@ -13,3 +15,12 @@ class UserViewSet(ModelViewSet):
 
 class AccountLogin(LoginView):
     template_name="registration/login.html"
+    
+class AccountMain(RedirectView):
+    pattern_name="accounts:profile"
+    
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse(self.pattern_name, kwargs={"pk":self.request.user.id})
+
+class AccountProfile(DetailView):
+    model=User
